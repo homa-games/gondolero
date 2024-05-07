f1dfe40977d175119cecb0a1791543c103384409
zhuk <>
2024-05-07 20:06:38 +0400

    chat screen with animation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,16 +17,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.myapplication.data.Message
+import com.example.myapplication.data.SampleData
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(Message(author = "Android", body = "body text"))
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -43,7 +55,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(message: Message) {
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Composable
+fun MessageCard(message: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
@@ -58,25 +79,41 @@ fun Greeting(message: Message) {
                 )
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) {
+                MaterialTheme.colorScheme.primary
+            } else {
+                MaterialTheme.colorScheme.surface
+            }
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = message.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleSmall,
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 4.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 4.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = message.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium,
                 )
             }
         }
     }
 }
-
-data class Message(val author: String, val body: String)
 
 @Preview(
     showBackground = true,
@@ -88,10 +125,18 @@ data class Message(val author: String, val body: String)
     name = "Dark Mode",
 )
 @Composable
-fun GreetingPreview() {
+fun MessageCardPreview() {
     MyApplicationTheme {
         Surface {
-            Greeting(Message(author = "Android", body = "test text body"))
+            MessageCard(Message(author = "Android", body = "test text body"))
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConversationPreview() {
+    MyApplicationTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
