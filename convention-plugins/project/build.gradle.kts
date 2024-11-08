9b1ad5940ccb5c2b91616fe62d007daa1a1a4e7d
zhuk <>
2024-11-08 20:54:38 +0400

    fix ktlint errors

--- convention-plugins/project/build.gradle.kts
+++ convention-plugins/project/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
 }
@@ -20,19 +17,23 @@ gradlePlugin {
     plugins {
         register("myAndroid.application.plugin") {
             id = "myAndroid.application.plugin"
-            implementationClass = "com.example.myapplication.conventionplugins.project.MyAndroidApplicationPlugin"
+            implementationClass =
+                "com.example.myapplication.conventionplugins.project.MyAndroidApplicationPlugin"
         }
         register("myAndroid.library.plugin") {
             id = "myAndroid.library.plugin"
-            implementationClass = "com.example.myapplication.conventionplugins.project.MyAndroidLibraryPlugin"
+            implementationClass =
+                "com.example.myapplication.conventionplugins.project.MyAndroidLibraryPlugin"
         }
         register("myOptions.compiler.plugin") {
             id = "myOptions.compiler.plugin"
-            implementationClass = "com.example.myapplication.conventionplugins.project.MyCompileOptionsPlugin"
+            implementationClass =
+                "com.example.myapplication.conventionplugins.project.MyCompileOptionsPlugin"
         }
         register("myKtlint.linter.plugin") {
             id = "myKtlint.linter.plugin"
-            implementationClass = "com.example.myapplication.conventionplugins.project.MyKtlintPlugin"
+            implementationClass =
+                "com.example.myapplication.conventionplugins.project.MyKtlintPlugin"
         }
     }
 }
--- convention-plugins/project/src/main/kotlin/com/example/myapplication/conventionplugins/project/extentions/BaseExtentions.kt
+++ convention-plugins/project/src/main/kotlin/com/example/myapplication/conventionplugins/project/extentions/BaseExtentions.kt
@@ -18,44 +18,43 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 private typealias CommonAndroidExtension = CommonExtension<
-        out BuildFeatures,
-        out BuildType,
-        out DefaultConfig,
-        out ProductFlavor,
-        out AndroidResources,
-        out Installation>
+    out BuildFeatures,
+    out BuildType,
+    out DefaultConfig,
+    out ProductFlavor,
+    out AndroidResources,
+    out Installation,
+>
 
 private val Project.libExt: LibraryExtension
     get() = extensions.findByType(LibraryExtension::class)
-        ?: error(
-            "\"Project.libExt\" value may be called only from android library gradle script"
-        )
+        ?: error("\"Project.libExt\" value may be called only from android library gradle script")
 
 private val Project.appExt: BaseAppModuleExtension
     get() = extensions.findByType(BaseAppModuleExtension::class)
         ?: error(
-            "\"Project.appExt\" value may be called only from android application gradle script"
+            "\"Project.appExt\" value may be called only from android application gradle script",
         )
 
 private val Project.commonExt: CommonAndroidExtension
     get() = extensions.findByType(BaseAppModuleExtension::class)
         ?: extensions.findByType(LibraryExtension::class)
         ?: error(
-            "\"Project.commonExt\" value may be called only from android application or library gradle script"
+            "\"Project.commonExt\" value may be called only from android application or library gradle script",
         )
 
 val Project.verLibs: LibrariesForLibs
     get() = extensions.getByType(LibrariesForLibs::class)
 
-fun Project.myAndroidLibConfig(block: LibraryExtension.() -> Unit): Unit {
+fun Project.myAndroidLibConfig(block: LibraryExtension.() -> Unit) {
     block(libExt)
 }
 
-fun Project.myAndroidAppConfig(block: BaseAppModuleExtension.() -> Unit): Unit {
+fun Project.myAndroidAppConfig(block: BaseAppModuleExtension.() -> Unit) {
     block(appExt)
 }
 
-fun Project.myCommonAndroidConfig(block: CommonAndroidExtension.() -> Unit): Unit {
+fun Project.myCommonAndroidConfig(block: CommonAndroidExtension.() -> Unit) {
     block(commonExt)
 }
 
