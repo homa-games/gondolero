10361db7140da2cde399bd06674d5003c9480a4d
zhuk <>
2025-07-06 16:34:28 +0400

    exclude common plugins from top-level build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.ktlintGradle) apply false
-    id("project.plugin") //fixme из-за этой строчки падает сборка через build - rebuild. собирать через 'gradlew :app:assembleDebug' или build - make project
+//    alias(libs.plugins.androidApplication) apply false
+//    alias(libs.plugins.androidLibrary) apply false
+//    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+//    alias(libs.plugins.ktlintGradle) apply false
+//    alias(libs.plugins.kotlin.compose) apply false
+    id("project.plugin")
 }
